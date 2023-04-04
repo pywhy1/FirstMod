115463294be128511b82b4afa19743cd816dda75
pywhy1 <norskinwater000@gmail.com>
2023-04-04 14:18:52 -0400

    Code and Item additions.
    Added Giant Grass Bow and Grass bow variants

--- Content/Items/Weapons/GiantGrassBow.cs
+++ Content/Items/Weapons/GiantGrassBow.cs
@@ -6,35 +6,35 @@ using Microsoft.Xna.Framework;
 
 namespace FirstMod.Content.Items.Weapons
 {
-    internal class GrassBow : ModItem
+    internal class GiantGrassBow : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Grass Bow"); //display name on the sword
-            Tooltip.SetDefault("Leaf + stick = Bow :D\nNimble and fast."); //description
+            DisplayName.SetDefault("Giant Grass Bow"); //display name on the sword
+            Tooltip.SetDefault("Leaf + stick = Bow :D\nHuge, big and bulky"); //description
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1; //how many needed for journey mode research
         }
         public override void SetDefaults()
         {
             //hitbox
 
-            Item.width = 16;
-            Item.height = 32;
+            Item.width = 32;
+            Item.height = 64;
 
             //use and animation style
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.useTime = 20;
-            Item.useAnimation = 20;
+            Item.useTime = 40;
+            Item.useAnimation = 40;
             Item.autoReuse = true;
             Item.shoot = 1;
             Item.useAmmo = AmmoID.Arrow;
-            Item.shootSpeed = 7f;
+            Item.shootSpeed = 4f;
 
             //damage
             Item.DamageType = DamageClass.Ranged;
-            Item.damage = 6;
-            Item.knockBack = 6f;
-            Item.crit = 10;
+            Item.damage = 20;
+            Item.knockBack = 4f;
+            Item.crit = 5;
 
             //misc
             Item.value = Item.buyPrice(gold: 3);
@@ -47,8 +47,8 @@ namespace FirstMod.Content.Items.Weapons
         public override void AddRecipes()
         {
             CreateRecipe() //this takes a param in the '()' for how many gets created at a time, by default it is 1.
-                .AddIngredient(ModContent.ItemType<FirstItem>(), 6)//this accesses my FirstItem material.
-                .AddRecipeGroup(RecipeGroupID.Wood, 10)
+                .AddIngredient(ModContent.ItemType<FirstItem>(), 20)//this accesses my FirstItem material.
+                .AddRecipeGroup(RecipeGroupID.Wood, 40)
                 //.AddIngredient(ItemID.Wood, 10) We use recipe group so I can use any wood in the crafting recipe
                 .AddTile(TileID.WorkBenches) //addtile takes tile id for crafting (like if it takes a anvil or a work bench)
                 .Register(); //register registers the item.
